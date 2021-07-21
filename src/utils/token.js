a014932eb0ea0082d63f63b14a07a0a7e603146c
abbasali1994 <meetabbasali1994@gmail.com>
2021-07-21 23:16:46 +0530

    Updated Token List (#63)

--- src/utils/token.js
+++ src/utils/token.js
@@ -5,6 +5,7 @@ let tokens = [
     tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
     tokenSymbol: "eth",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "1INCH",
@@ -12,6 +13,7 @@ let tokens = [
     tokenAddress: "0x111111111117dc0aa78b770fa6a738034120c302",
     tokenSymbol: "1inch",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "AAVE",
@@ -19,20 +21,23 @@ let tokens = [
     tokenAddress: "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9",
     tokenSymbol: "aave",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "ALCHEMIX",
     tokenDisplayName: "Alchemix",
-    tokenAddress: "0xbc6da0fe9ad5f3b0d58160288917aa56653660e9",
+    tokenAddress: "0xdBdb4d16EdA451D0503b854CF79D55697F90c8DF",
     tokenSymbol: "alcx",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "ALPHA",
     tokenDisplayName: "Alpha Finance",
-    tokenAddress: "0xa1faa113cbe53436df28ff0aee54275c13b40975",
+    tokenAddress: "0x969b707B43B2A5218E1Da2f3a28A4129e43D47D6",
     tokenSymbol: "alpha",
     tokenBal: 0,
+    tokenDecimals: 0,
   },
   {
     tokenName: "AMP",
@@ -40,6 +45,7 @@ let tokens = [
     tokenAddress: "0xff20817765cb7f73d4bde2e66e067e58d11095c2",
     tokenSymbol: "amp",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "AMPL",
@@ -47,6 +53,7 @@ let tokens = [
     tokenAddress: "0xd46ba6d942050d489dbd938a2c909a5d5039a161",
     tokenSymbol: "ampl",
     tokenBal: 0,
+    tokenDecimals: 9,
   },
   {
     tokenName: "ANT",
@@ -54,6 +61,7 @@ let tokens = [
     tokenAddress: "0xa117000000f279d81a1d3cc75430faa017fa5a2e",
     tokenSymbol: "ant",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "AXS",
@@ -61,6 +69,7 @@ let tokens = [
     tokenAddress: "0xbb0e17ef65f82ab018d8edd776e8dd940327b28b",
     tokenSymbol: "axs",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "BADGER",
@@ -68,6 +77,7 @@ let tokens = [
     tokenAddress: "0x3472a5a71965499acd81997a54bba8d852c6e53d",
     tokenSymbol: "badger",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "BAL",
@@ -75,6 +85,7 @@ let tokens = [
     tokenAddress: "0xba100000625a3754423978a60c9317c58a424e3d",
     tokenSymbol: "bal",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "BAND",
@@ -82,6 +93,7 @@ let tokens = [
     tokenAddress: "0xba11d00c5f74255f56a5e366f4f77f5a186d7f55",
     tokenSymbol: "band",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "BASKET DAO",
@@ -89,6 +101,7 @@ let tokens = [
     tokenAddress: "0x44564d0bd94343f72e3c8a0d22308b7fa71db0bb",
     tokenSymbol: "bask",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "BNT",
@@ -96,6 +109,7 @@ let tokens = [
     tokenAddress: "0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c",
     tokenSymbol: "bnt",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "BOND",
@@ -103,6 +117,7 @@ let tokens = [
     tokenAddress: "0x0391d2021f89dc339f60fff84546ea23e337750f",
     tokenSymbol: "bond",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "COMBO",
@@ -110,6 +125,7 @@ let tokens = [
     tokenAddress: "0xffffffff2ba8f66d4e51811c5190992176930278",
     tokenSymbol: "combo",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "COMP",
@@ -117,6 +133,7 @@ let tokens = [
     tokenAddress: "0xc00e94cb662c3520282e6f5717214004a7f26888",
     tokenSymbol: "comp",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "CORE",
@@ -124,6 +141,7 @@ let tokens = [
     tokenAddress: "0x62359ed7505efc61ff1d56fef82158ccaffa23d7",
     tokenSymbol: "core",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "CVC",
@@ -131,6 +149,7 @@ let tokens = [
     tokenAddress: "0x41e5560054824ea6b0732e656e3ad64e20e94e45",
     tokenSymbol: "cvc",
     tokenBal: 0,
+    tokenDecimals: 8,
   },
   {
     tokenName: "CREAM",
@@ -138,6 +157,7 @@ let tokens = [
     tokenAddress: "0x2ba592f78db6436527729929aaf6c908497cb200",
     tokenSymbol: "cream",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "CRV",
@@ -145,6 +165,7 @@ let tokens = [
     tokenAddress: "0xd533a949740bb3306d119cc777fa900ba034cd52",
     tokenSymbol: "crv",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "DAI",
@@ -152,6 +173,7 @@ let tokens = [
     tokenAddress: "0x6b175474e89094c44da98b954eedeac495271d0f",
     tokenSymbol: "dai",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "DNT",
@@ -159,6 +181,7 @@ let tokens = [
     tokenAddress: "0x0abdace70d3790235af448c88547603b945604ea",
     tokenSymbol: "dnt",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "DOUGH",
@@ -166,6 +189,7 @@ let tokens = [
     tokenAddress: "0xad32a8e6220741182940c5abf610bde99e737b2d",
     tokenSymbol: "dough",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "DPI",
@@ -173,6 +197,7 @@ let tokens = [
     tokenAddress: "0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b",
     tokenSymbol: "dpi",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "DPX",
@@ -180,6 +205,7 @@ let tokens = [
     tokenAddress: "0xeec2be5c91ae7f8a338e1e5f3b5de49d07afdc81",
     tokenSymbol: "dpx",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "rDPX",
@@ -187,6 +213,7 @@ let tokens = [
     tokenAddress: "0x0ff5a8451a839f5f0bb3562689d9a44089738d11",
     tokenSymbol: "rdpx",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "FARM",
@@ -194,6 +221,7 @@ let tokens = [
     tokenAddress: "0xa0246c9032bc3a600820415ae600c6388619a14d",
     tokenSymbol: "farm",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MEME",
@@ -202,6 +230,7 @@ let tokens = [
     tokenAddress: "0xd5525d397898e5502075ea5e830d8914f6f0affe",
     tokenSymbol: "meme",
     tokenBal: 0,
+    tokenDecimals: 8,
   },
   {
     tokenName: "GNO",
@@ -209,6 +238,7 @@ let tokens = [
     tokenAddress: "0x6810e776880c02933d47db1b9fc05908e5386b96",
     tokenSymbol: "gno",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "GRT",
@@ -216,6 +246,7 @@ let tokens = [
     tokenAddress: "0xc944e90c64b2c07662a292be6244bdf05cda44a7",
     tokenSymbol: "grt",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "HEGIC",
@@ -223,13 +254,15 @@ let tokens = [
     tokenAddress: "0x584bc13c7d411c00c01a62e8019472de68768430",
     tokenSymbol: "hegic",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "KP3R",
     tokenDisplayName: "Keep3rV1",
-    tokenAddress: "0x33518A786e2D6a40a2c78f2431A9385a32ca36D5",
+    tokenAddress: "0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44",
     tokenSymbol: "kp3r",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "KEEP",
@@ -237,6 +270,7 @@ let tokens = [
     tokenAddress: "0x85eee30c52b0b379b046fb0f85f4f3dc3009afec",
     tokenSymbol: "keep",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "KNC",
@@ -244,6 +278,7 @@ let tokens = [
     tokenAddress: "0xdefa4e8a7bcba345f687a2f1456f5edd9ce97202",
     tokenSymbol: "knc",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "LDO",
@@ -251,6 +286,7 @@ let tokens = [
     tokenAddress: "0x5a98fcbea516cf06857215779fd812ca3bef1b32",
     tokenSymbol: "ldo",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "LINK",
@@ -258,6 +294,7 @@ let tokens = [
     tokenAddress: "0x514910771af9ca656af840dff83e8264ecf986ca",
     tokenSymbol: "link",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "LOOM",
@@ -265,6 +302,7 @@ let tokens = [
     tokenAddress: "0x42476f744292107e34519f9c357927074ea3f75d",
     tokenSymbol: "loom",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "LRC",
@@ -272,6 +310,7 @@ let tokens = [
     tokenAddress: "0xbbbbca6a901c926f240b89eacb641d8aec7aeafd",
     tokenSymbol: "lrc",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MANA",
@@ -279,6 +318,7 @@ let tokens = [
     tokenAddress: "0x0f5d2fb29fb7d3cfee444a200298f468908cc942",
     tokenSymbol: "mana",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MBBASED",
@@ -286,6 +326,7 @@ let tokens = [
     tokenAddress: "0x26cf82e4ae43d31ea51e72b663d26e26a75af729",
     tokenSymbol: "mbbased",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MBTC",
@@ -293,6 +334,7 @@ let tokens = [
     tokenAddress: "0x945facb997494cc2570096c74b5f66a3507330a1",
     tokenSymbol: "mbtc",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MIR",
@@ -300,6 +342,7 @@ let tokens = [
     tokenAddress: "0x09a3ecafa817268f77be1283176b946c4ff2e608",
     tokenSymbol: "mir",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MBABA",
@@ -307,6 +350,7 @@ let tokens = [
     tokenAddress: "0x56aa298a19c93c6801fdde870fa63ef75cc0af72",
     tokenSymbol: "mbaba",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MNFLX",
@@ -314,6 +358,7 @@ let tokens = [
     tokenAddress: "0xc8d674114bac90148d11d3c1d33c61835a0f9dcd",
     tokenSymbol: "mnflx",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MTSLA",
@@ -321,6 +366,7 @@ let tokens = [
     tokenAddress: "0x21ca39943e91d704678f5d00b6616650f066fd63",
     tokenSymbol: "mtsla",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MTWTR",
@@ -328,6 +374,7 @@ let tokens = [
     tokenAddress: "0xedb0414627e6f1e3f082de65cd4f9c693d78cca9",
     tokenSymbol: "mtwtr",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MVIXY",
@@ -335,6 +382,7 @@ let tokens = [
     tokenAddress: "0xf72fcd9dcf0190923fadd44811e240ef4533fc86",
     tokenSymbol: "mvixy",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MKR",
@@ -342,6 +390,7 @@ let tokens = [
     tokenAddress: "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
     tokenSymbol: "mkr",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MLN",
@@ -349,6 +398,7 @@ let tokens = [
     tokenAddress: "0xec67005c4e498ec7f55e092bd1d35cbc47c91892",
     tokenSymbol: "mln",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MPH",
@@ -356,6 +406,7 @@ let tokens = [
     tokenAddress: "0x8888801af4d980682e47f1a9036e589479e835c5",
     tokenSymbol: "mph",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "MTA",
@@ -364,6 +415,7 @@ let tokens = [
     tokenAddress: "0xa3bed4e1c75d00fa6f4e5e6922db7261b5e9acd2",
     tokenSymbol: "mta",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "NMR",
@@ -371,6 +423,7 @@ let tokens = [
     tokenAddress: "0x1776e1f26f98b1a5df9cd347953a26dd3cb46671",
     tokenSymbol: "nmr",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "NU",
@@ -378,6 +431,7 @@ let tokens = [
     tokenAddress: "0x4fe83213d56308330ec302a8bd641f1d0113a4cc",
     tokenSymbol: "nu",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "OLYMPUS",
@@ -385,6 +439,7 @@ let tokens = [
     tokenAddress: "0x383518188c0c6d7730d91b2c03a03c837814a899",
     tokenSymbol: "ohm",
     tokenBal: 0,
+    tokenDecimals: 9,
   },
   {
     tokenName: "OPIUM",
@@ -392,6 +447,7 @@ let tokens = [
     tokenAddress: "0x888888888889c00c67689029d7856aac1065ec11",
     tokenSymbol: "opium",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "OUSD",
@@ -399,6 +455,7 @@ let tokens = [
     tokenAddress: "0x2a8e1e676ec238d8a992307b495b45b3feaa5e86",
     tokenSymbol: "ousd",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "OXT",
@@ -406,6 +463,7 @@ let tokens = [
     tokenAddress: "0x4575f41308ec1483f3d399aa9a2826d74da13deb",
     tokenSymbol: "oxt",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "PICKLE",
@@ -413,6 +471,7 @@ let tokens = [
     tokenAddress: "0x429881672b9ae42b8eba0e26cd9c73711b891ca5",
     tokenSymbol: "pickle",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "PREMIA",
@@ -420,6 +479,7 @@ let tokens = [
     tokenAddress: "0x6399c842dd2be3de30bf99bc7d1bbf6fa3650e70",
     tokenSymbol: "premia",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "REN",
@@ -427,13 +487,15 @@ let tokens = [
     tokenAddress: "0x408e41876cccdc0f92210600ef50372656052a38",
     tokenSymbol: "ren",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "REPV2",
     tokenDisplayName: "Reputation",
-    tokenAddress: "0x1915cE14Bc5F0A4E9742D55b5f251ED71f4f3e96",
+    tokenAddress: "0x221657776846890989a759ba2973e427dff5c9bb",
     tokenSymbol: "repv2",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "RGT",
@@ -441,6 +503,7 @@ let tokens = [
     tokenAddress: "0xd291e7a03283640fdc51b121ac401383a46cc623",
     tokenSymbol: "rgt",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "RUNE",
@@ -449,6 +512,7 @@ let tokens = [
     tokenAddress: "0x3155ba85d5f96b2d030a4966af206230e46849cb",
     tokenSymbol: "rune",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "SNX",
@@ -457,6 +521,7 @@ let tokens = [
     tokenAddress: "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f",
     tokenSymbol: "snx",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "SPANK",
@@ -464,6 +529,7 @@ let tokens = [
     tokenAddress: "0x42d6622dece394b54999fbd73d108123806f6a18",
     tokenSymbol: "spank",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "SRM",
@@ -471,6 +537,7 @@ let tokens = [
     tokenAddress: "0x476c5e26a75bd202a9683ffd34359c0cc15be0ff",
     tokenSymbol: "srm",
     tokenBal: 0,
+    tokenDecimals: 6,
   },
   {
     tokenName: "STAKE",
@@ -478,6 +545,7 @@ let tokens = [
     tokenAddress: "0x0ae055097c6d159879521c384f1d2123d1f195e6",
     tokenSymbol: "stake",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "SUSD",
@@ -486,6 +554,7 @@ let tokens = [
     tokenAddress: "0x57ab1ec28d129707052df4df418d58a2d46d5f51",
     tokenSymbol: "susd",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "SUSHI",
@@ -493,6 +562,7 @@ let tokens = [
     tokenAddress: "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2",
     tokenSymbol: "sushi",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "STORJ",
@@ -500,6 +570,7 @@ let tokens = [
     tokenAddress: "0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac",
     tokenSymbol: "storj",
     tokenBal: 0,
+    tokenDecimals: 8,
   },
   {
     tokenName: "TBTC",
@@ -507,6 +578,7 @@ let tokens = [
     tokenAddress: "0x8dAEBADE922dF735c38C80C7eBD708Af50815fAa",
     tokenSymbol: "tbtc",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "TORN",
@@ -514,6 +586,7 @@ let tokens = [
     tokenAddress: "0x77777feddddffc19ff86db637967013e6c6a116c",
     tokenSymbol: "torn",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "TUSD",
@@ -521,6 +594,7 @@ let tokens = [
     tokenAddress: "0x0000000000085d4780b73119b644ae5ecd22b376",
     tokenSymbol: "tusd",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "UMA",
@@ -528,6 +602,7 @@ let tokens = [
     tokenAddress: "0x04fa0d235c4abf4bcf4787af4cf447de572ef828",
     tokenSymbol: "uma",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "UNI",
@@ -535,6 +610,7 @@ let tokens = [
     tokenAddress: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
     tokenSymbol: "uni",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "USDC",
@@ -542,6 +618,7 @@ let tokens = [
     tokenAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
     tokenSymbol: "usdc",
     tokenBal: 0,
+    tokenDecimals: 6,
   },
   {
     tokenName: "UST",
@@ -549,6 +626,7 @@ let tokens = [
     tokenAddress: "0xa47c8bf37f92abed4a126bda807a7b7498661acd",
     tokenSymbol: "ust",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "USDT",
@@ -556,6 +634,7 @@ let tokens = [
     tokenAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
     tokenSymbol: "usdt",
     tokenBal: 0,
+    tokenDecimals: 6,
   },
   {
     tokenName: "WNXM",
@@ -563,6 +642,7 @@ let tokens = [
     tokenAddress: "0x0d438f3b5175bebc262bf23753c1e53d03432bde",
     tokenSymbol: "wnxm",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "YFI",
@@ -570,6 +650,7 @@ let tokens = [
     tokenAddress: "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e",
     tokenSymbol: "yfi",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "XSUSHI",
@@ -577,6 +658,7 @@ let tokens = [
     tokenAddress: "0x8798249c2e607446efb7ad49ec89dd1865ff4272",
     tokenSymbol: "xsushi",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "YAM",
@@ -584,6 +666,7 @@ let tokens = [
     tokenAddress: "0x0aacfbec6a24756c20d41914f2caba817c0d8521",
     tokenSymbol: "yam",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "YETI",
@@ -591,6 +674,7 @@ let tokens = [
     tokenAddress: "0xb4bebd34f6daafd808f73de0d10235a92fbb6c3d",
     tokenSymbol: "yeti",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "YLD",
@@ -598,6 +682,7 @@ let tokens = [
     tokenAddress: "0xf94b5c5651c888d928439ab6514b93944eee6f48",
     tokenSymbol: "yld",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "ZHEGIC",
@@ -605,6 +690,7 @@ let tokens = [
     tokenAddress: "0x837010619aeb2ae24141605afc8f66577f6fb2e7",
     tokenSymbol: "zhegic",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "ZLOT",
@@ -612,6 +698,7 @@ let tokens = [
     tokenAddress: "0xa8e7ad77c60ee6f30bac54e2e7c0617bd7b5a03e",
     tokenSymbol: "zlot",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
   {
     tokenName: "ZRX",
@@ -619,6 +706,7 @@ let tokens = [
     tokenAddress: "0xe41d2489571d322189246dafa5ebde1f4699f498",
     tokenSymbol: "zrx",
     tokenBal: 0,
+    tokenDecimals: 18,
   },
 ];
 
@@ -654,35 +742,39 @@ export const getAllTokens = () => {
 export const getPriceId = (token) => {
   const tokenName = token.tokenDisplayName;
   const tokenCoingeckoID = token.tokenCoingeckoID;
-  const id = tokenCoingeckoID !== undefined ? tokenCoingeckoID: tokenName.toLowerCase().replace(' ','-').replace(' ','-').replace(' ','-').replace('.','-');
+  const id =
+    tokenCoingeckoID !== undefined
+      ? tokenCoingeckoID
+      : tokenName
+          .toLowerCase()
+          .replace(" ", "-")
+          .replace(" ", "-")
+          .replace(" ", "-")
+          .replace(".", "-");
   return id;
 };
 
 export const getCoinGeckoTokenIDS = () => {
   let coingeckoIDS = "";
   tokens.forEach((token) => {
-    coingeckoIDS += getPriceId(token) + ","
+    coingeckoIDS += getPriceId(token) + ",";
   });
 
-  coingeckoIDS = coingeckoIDS.slice(0, -1)
+  coingeckoIDS = coingeckoIDS.slice(0, -1);
   return coingeckoIDS;
-}
+};
 
 export const getCoinGeckoTokenIDAndTokenSymbol = () => {
-
   let tokenDetails = {};
 
   tokens.forEach((token) => {
-   
     const priceId = getPriceId(token);
     const symbol = token.tokenSymbol;
 
     tokenDetails[priceId] = symbol;
   });
 
-
   return tokenDetails;
-
 };
 
 export const getTokenCount = () => tokens.length;
